368ad234467bb4ad5a18712edc94a9039784befc
Jimy Huacho Dolores <jimmyhd_19@hotmail.com>
2021-04-24 21:11:58 -0500

    cambios master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,17 @@ import { AppComponent } from './app.component';
 import { MyInterceptorService } from './service/my-interceptor.service';
 import { RequestHtppService } from './service/request-htpp.service';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [
     RequestHtppService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: MyInterceptorService,
-      multi: true
-    }
+      multi: true,
+    },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
